0dbdf7a6314ef2d364deafbd2ce5a3d00f2f7b7a
Milton Munoz <livemilton@gmail.com>
2025-08-13 21:45:36 -0500

    maps-app :sparkles: show markers list with dinamyc view

--- src/app/pages/markers-page/markers-page.component.ts
+++ src/app/pages/markers-page/markers-page.component.ts
@@ -1,7 +1,8 @@
 import { AfterViewInit, Component, ElementRef, signal, viewChild } from '@angular/core';
-import mapboxgl, { MapMouseEvent } from 'mapbox-gl';
+import mapboxgl, { LngLat, LngLatLike, MapMouseEvent } from 'mapbox-gl';
 import { environment } from '../../../environments/environment';
 import { v4 as UUIDv4} from 'uuid';
+import { JsonPipe } from '@angular/common';
 
 mapboxgl.accessToken = environment.mapboxKey;
 
@@ -12,7 +13,7 @@ interface Marker {
 
 @Component({
   selector: 'app-markers-page',
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './markers-page.component.html',
 })
 export class MarkersPageComponent implements AfterViewInit{
@@ -81,6 +82,16 @@ export class MarkersPageComponent implements AfterViewInit{
     this.markers.update((markers) => [newMarker, ...markers]);
 
     console.log(this.markers());
+  }
+
+
+  //go to marker
+  flyToMarker(lngLat: LngLatLike){
+    if(!this.map()) return;
+
+    this.map()?.flyTo({
+      center: lngLat,
+    });
   }
 
  }
